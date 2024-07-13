91bb263721472825a35f2e3125eb92b92056c600
Josh Bain <joshuambain@gmail.com>
2024-07-13 13:31:25 -0400

    complete dom modifying elements lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+document.querySelector("main#main").remove(); 
+//document.getElementById("main").remove();
+
+const newHeader = document.createElement("H1");
+newHeader.setAttribute('id','victory');
+newHeader.textContent = "Josh Bain is the champion";
